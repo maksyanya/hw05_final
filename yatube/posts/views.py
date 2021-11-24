eaa78f6d7ebea8374ce3d3d523a5bb43f2dfe321
Максим Арзамасцев <kyhnya@yandex.ru>
2021-11-24 22:23:22 +0300

    Before_revie

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -57,7 +57,7 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
-    comments = post.comments.filter(post=post).all()
+    comments = post.comments.all()
     context = {
         'post': post,
         'comments': comments,
