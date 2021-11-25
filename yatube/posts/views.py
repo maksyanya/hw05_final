4722092afca79e5f836499d9224f0e3c77aba33c
Максим Арзамасцев <kyhnya@yandex.ru>
2021-11-25 09:55:31 +0300

    Before_check_review

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -57,9 +57,11 @@ def profile(request, username):
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+    form = CommentForm(request.POST or None)
     comments = post.comments.select_related('author')
     context = {
         'post': post,
+        'form': form,
         'comments': comments
     }
     return render(request, 'posts/post_detail.html', context)
