78c0efb5f1205575c4bc83317666f610155a9d69
Максим Арзамасцев <kyhnya@yandex.ru>
2021-11-29 19:48:28 +0300

    After_002_review

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -99,6 +99,6 @@ class Follow(models.Model):
         verbose_name_plural = "Подписки"
         constraints = [
             models.UniqueConstraint(
-                fields=["user", "author"], name="unique_follow"
+                fields=["user", "author"], name="unique_follow",
             )
         ]
